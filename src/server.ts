06f0c5bec7e8a66e7a4933025e6ad6299002d831
Diego Pais <drborges.pais@gmail.com>
2025-09-07 13:53:33 -0300

    feat: configuring typeform

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import { app } from './app';
+import { AppDataSource } from './data-source';
+
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization:', err);
+  });
+
+
